454aa49b1d35d3ea933d05900a85d1ccd81e4739
Sander Mertens <sander.mertens8@gmail.com>
2019-04-14 06:59:17 -0700

    Revise code to use CASCADE columns

--- src/main.c
+++ src/main.c
@@ -10,52 +10,38 @@ void EcsAddMatTransform2D(ecs_rows_t *rows) {
     }
 }
 
-void EcsInitTransformChildren2D(ecs_rows_t *rows) {
-    EcsMatTransform2D *m = ecs_column(rows, EcsMatTransform2D, 1);
-    EcsMatTransform2D *m_container = ecs_column(rows, EcsMatTransform2D, 2);
-
-    int i;
-    for (i = 0; i < rows->count; i ++) {
-        m[i] = *m_container;
-    }
-}
-
-void EcsApplyTranslation2D(ecs_rows_t *rows) {
+void EcsApplyTransform2D(ecs_rows_t *rows) {
     EcsMatTransform2D *m = ecs_column(rows, EcsMatTransform2D, 1);
     EcsPosition2D *p = ecs_column(rows, EcsPosition2D, 2);
-    
+    EcsRotation2D *r = ecs_column_test(rows, EcsRotation2D, 3);
+    EcsScale2D *s = ecs_column_test(rows, EcsScale2D, 4);
+    EcsMatTransform2D *m_parent = ecs_shared_test(rows, EcsMatTransform2D, 5);
     int i;
+
+    if (!m_parent) {
+        for (i = 0; i < rows->count; i ++) {
+            m[i] = ECS_MAT3X3_IDENTITY;
+        }
+    } else {
+        for (i = 0; i < rows->count; i ++) {
+            m[i] = *m_parent;
+        }
+    }
+
     for (i = 0; i < rows->count; i ++) {
         ecs_mat3x3_add_translation(&m[i], &p[i]);
     }
-}
 
-void EcsApplyRotation2D(ecs_rows_t *rows) {
-    EcsMatTransform2D *m = ecs_column(rows, EcsMatTransform2D, 1);
-    EcsRotation2D *r = ecs_column(rows, EcsRotation2D, 2);
-
-    int i;
-    for (i = 0; i < rows->count; i ++) {
-        ecs_mat3x3_add_rotation(&m[i], r[i].angle);
+    if (r) {
+        for (i = 0; i < rows->count; i ++) {
+            ecs_mat3x3_add_rotation(&m[i], r[i].angle);
+        }
     }
-}
 
-void EcsApplyScaling2D(ecs_rows_t *rows) {
-    EcsMatTransform2D *m = ecs_column(rows, EcsMatTransform2D, 1);
-    EcsScale2D *s = ecs_column(rows, EcsScale2D, 2);
-
-    int i;
-    for (i = 0; i < rows->count; i ++) {
-        ecs_mat3x3_add_scale(&m[i], &s[i]);
-    }
-}
-
-void EcsResetTransform2D(ecs_rows_t *rows) {
-    EcsMatTransform2D *m = ecs_column(rows, EcsMatTransform2D, 1);
-
-    int i;
-    for (i = 0; i < rows->count; i ++) {
-        m[i] = ECS_MAT3X3_IDENTITY;
+    if (s) {
+        for (i = 0; i < rows->count; i ++) {
+            ecs_mat3x3_add_scale(&m[i], &s[i]);
+        }
     }
 }
 
@@ -69,25 +55,22 @@ void EcsSystemsTransform(
     ECS_IMPORT(world, EcsComponentsTransform, flags);
 
     /* System that adds transform matrix to every entity with transformations */
-    ECS_SYSTEM(world, EcsAddMatTransform2D, EcsOnLoad, EcsPosition2D | EcsRotation2D | EcsScale2D, !EcsMatTransform2D, SYSTEM.EcsHidden);
+    ECS_SYSTEM(world, EcsAddMatTransform2D, EcsOnLoad, 
+        EcsPosition2D | EcsRotation2D | EcsScale2D, 
+        !EcsMatTransform2D, 
+        SYSTEM.EcsHidden);
 
-    /* Systems that add transformations to transform matrix */
-    ECS_SYSTEM(world, EcsApplyTranslation2D, EcsOnValidate, EcsMatTransform2D, EcsPosition2D, SYSTEM.EcsHidden);
-    ECS_SYSTEM(world, EcsApplyRotation2D, EcsOnValidate, EcsMatTransform2D, EcsRotation2D, SYSTEM.EcsHidden);
-    ECS_SYSTEM(world, EcsApplyScaling2D, EcsOnValidate, EcsMatTransform2D, EcsScale2D, SYSTEM.EcsHidden);
+    ECS_SYSTEM(world, EcsApplyTransform2D, EcsOnValidate, 
+        EcsMatTransform2D,
+        EcsPosition2D,
+        ?EcsRotation2D,
+        ?EcsScale2D,
+        CASCADE.EcsMatTransform2D,
+        SYSTEM.EcsHidden);
 
-    /* Copy transformation from parent to child entities */
-    ECS_SYSTEM(world, EcsInitTransformChildren2D, EcsPreUpdate, EcsMatTransform2D, CONTAINER.EcsMatTransform2D, SYSTEM.EcsHidden);
-
-    /* Reset transformation */
-    ECS_SYSTEM(world, EcsResetTransform2D, EcsPreUpdate, EcsMatTransform2D, SYSTEM.EcsHidden);
-
-    ECS_TYPE(world, EcsTransform2D, 
-        EcsApplyTranslation2D, 
-        EcsApplyRotation2D, 
-        EcsApplyScaling2D, 
-        EcsInitTransformChildren2D, 
-        EcsResetTransform2D);
+    ECS_TYPE(world, EcsTransform2D,  
+        EcsAddMatTransform2D, 
+        EcsApplyTransform2D);
 
     ECS_SET_ENTITY(handles, EcsTransform2D);
 }
