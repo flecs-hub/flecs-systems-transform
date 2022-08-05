adacef3dd34b3c20e4512be52f9799058c4acd2f
Sander Mertens <sander.mertens8@gmail.com>
2022-08-04 21:52:25 -0700

    Fix interop with C++

--- include/flecs_systems_transform.h
+++ include/flecs_systems_transform.h
@@ -21,10 +21,13 @@ void FlecsSystemsTransformImport(
 namespace flecs {
 namespace systems {
 
-class transform : FlecsSystemsTransform {
+class transform {
 public:
     transform(flecs::world& ecs) {
+        // Load module contents
         FlecsSystemsTransformImport(ecs);
+
+        // Bind module contents with C++ types
         ecs.module<flecs::systems::transform>();
     }
 };
