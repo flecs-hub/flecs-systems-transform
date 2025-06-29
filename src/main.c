d92e0355b1133fd426b5596aa5e818f6c88e6c13
Sander Mertens <sander.mertens8@gmail.com>
2025-06-29 16:07:08 -0700

    Flecs v4.1

--- src/main.c
+++ src/main.c
@@ -1,38 +1,16 @@
 #include <flecs_systems_transform.h>
 
-void EcsAddTransform3(ecs_iter_t *it) {
-    ecs_world_t *world = it->world;
-    ecs_entity_t comp = ecs_field_id(it, 1);
-
-    int i;
-    for (i = 0; i < it->count; i ++) {
-        ecs_add_id(world, it->entities[i], comp);
-    }
-}
-
-void EcsApplyTransform3(ecs_iter_t *it) {
-    // Iterate, only table field is populated to reduce overhead
-    while (ecs_query_next_table(it)) {
-        // If table has not changed skip it, no time was wasted on populating
-        // iterator fields that aren't used.
-        if (!ecs_query_changed(NULL, it)) {
-            ecs_query_skip(it);
-            continue;
-        }
-
-        // Table has changed, populate fields so that we can run the system
-        // code as usual
-        ecs_query_populate(it);
-
-        EcsTransform3 *m = ecs_field(it, EcsTransform3, 1);
-        EcsTransform3 *m_parent = ecs_field(it, EcsTransform3, 2);
-        EcsPosition3 *p = ecs_field(it, EcsPosition3, 3);
-        EcsRotation3 *r = ecs_field(it, EcsRotation3, 4);
-        EcsScale3 *s = ecs_field(it, EcsScale3, 5);
+static void transform(ecs_iter_t *it) {
+    while (ecs_query_next(it)) {
+        EcsTransform3 *m = ecs_field(it, EcsTransform3, 0);
+        EcsTransform3 *m_parent = ecs_field(it, EcsTransform3, 1);
+        EcsPosition3 *p = ecs_field(it, EcsPosition3, 2);
+        EcsRotation3 *r = ecs_field(it, EcsRotation3, 3);
+        EcsScale3 *s = ecs_field(it, EcsScale3, 4);
         int i;
 
         if (!m_parent) {
-            if (ecs_field_is_self(it, 3)) {
+            if (ecs_field_is_self(it, 2)) {
                 for (i = 0; i < it->count; i ++) {
                     glm_translate_make(m[i].value, *(vec3*)&p[i]);
                 }
@@ -42,7 +20,7 @@ void EcsApplyTransform3(ecs_iter_t *it) {
                 }
             }
         } else {
-            if (ecs_field_is_self(it, 3)) {
+            if (ecs_field_is_self(it, 2)) {
                 for (i = 0; i < it->count; i ++) {
                     glm_translate_to(m_parent[0].value, *(vec3*)&p[i], m[i].value);
                 }
@@ -54,7 +32,7 @@ void EcsApplyTransform3(ecs_iter_t *it) {
         }
 
         if (r) {
-            if (ecs_field_is_self(it, 4)) {
+            if (ecs_field_is_self(it, 3)) {
                 for (i = 0; i < it->count; i ++) {
                     glm_rotate(m[i].value, r[i].x, (vec3){1.0, 0.0, 0.0});
                     glm_rotate(m[i].value, r[i].y, (vec3){0.0, 1.0, 0.0});
@@ -77,6 +55,15 @@ void EcsApplyTransform3(ecs_iter_t *it) {
     }
 }
 
+void EcsApplyTransform3(ecs_iter_t *it) {
+    transform(it);
+}
+
+void EcsApplyTransformOnce3(ecs_iter_t *it) {
+    transform(it);
+    ecs_remove_all(it->world, EcsTransformNeeded);
+}
+
 void FlecsSystemsTransformImport(
     ecs_world_t *world)
 {
@@ -85,47 +72,86 @@ void FlecsSystemsTransformImport(
 
     ecs_set_name_prefix(world, "Ecs");
 
-    /* System that adds transform matrix to every entity with transformations */
-    ECS_SYSTEM(world, EcsAddTransform3, EcsPostLoad,
-        [out] !flecs.components.transform.Transform3,
-        [filter] flecs.components.transform.Position3(self|up) || 
-        [filter] flecs.components.transform.Rotation3(self|up) || 
-        [filter] flecs.components.transform.Scale3(self|up));
+    ecs_add_pair(world, ecs_id(EcsPosition3), EcsWith, ecs_id(EcsTransform3));
+    ecs_add_pair(world, ecs_id(EcsRotation3), EcsWith, ecs_id(EcsTransform3));
+    ecs_add_pair(world, ecs_id(EcsScale3),    EcsWith, ecs_id(EcsTransform3));
 
     ecs_system(world, {
         .entity = ecs_entity(world, { 
             .name = "EcsApplyTransform3",
-            .add = { ecs_dependson(EcsOnValidate) }
+            .add = ecs_ids( ecs_dependson(EcsOnValidate) )
         }),
         .query = {
-            .filter = {
-                .terms = {{ 
-                    .id = ecs_id(EcsTransform3), 
-                    .inout = EcsOut 
-                },
-                {
-                    .id = ecs_id(EcsTransform3), 
-                    .inout = EcsIn,
-                    .oper = EcsOptional,
-                    .src.flags = EcsParent | EcsCascade
-                },
-                {
-                    .id = ecs_id(EcsPosition3),
-                    .inout = EcsIn
-                },
-                {
-                    .id = ecs_id(EcsRotation3),
-                    .inout = EcsIn,
-                    .oper = EcsOptional
-                },
-                {
-                    .id = ecs_id(EcsScale3),
-                    .inout = EcsIn,
-                    .oper = EcsOptional
-                }},
-                .instanced = true
+            .terms = {{ 
+                .id = ecs_id(EcsTransform3),
+                .inout = EcsOut,
             },
+            {
+                .id = ecs_id(EcsTransform3), 
+                .inout = EcsIn,
+                .oper = EcsOptional,
+                .src.id = EcsCascade
+            },
+            {
+                .id = ecs_id(EcsPosition3),
+                .inout = EcsIn
+            },
+            {
+                .id = ecs_id(EcsRotation3),
+                .inout = EcsIn,
+                .oper = EcsOptional
+            },
+            {
+                .id = ecs_id(EcsScale3),
+                .inout = EcsIn,
+                .oper = EcsOptional
+            },
+            {
+                .id = EcsTransformManually,
+                .oper = EcsNot
+            },
+            {
+                .id = EcsTransformOnce,
+                .oper = EcsNot
+            }}
         },
         .run = EcsApplyTransform3
+    });
+
+    ecs_system(world, {
+        .entity = ecs_entity(world, { 
+            .name = "EcsApplyTransformOnce3",
+            .add = ecs_ids( ecs_dependson(EcsOnValidate) )
+        }),
+        .query = {
+            .terms = {{ 
+                .id = ecs_id(EcsTransform3),
+                .inout = EcsOut,
+            }, {
+                .id = ecs_id(EcsTransform3), 
+                .inout = EcsIn,
+                .oper = EcsOptional,
+                .src.id = EcsCascade
+            }, {
+                .id = ecs_id(EcsPosition3),
+                .inout = EcsIn
+            }, {
+                .id = ecs_id(EcsRotation3),
+                .inout = EcsIn,
+                .oper = EcsOptional
+            }, {
+                .id = ecs_id(EcsScale3),
+                .inout = EcsIn,
+                .oper = EcsOptional
+            }, {
+                .id = EcsTransformOnce
+            }, {
+                .id = EcsTransformNeeded
+            }, {
+                .id = EcsTransformManually,
+                .oper = EcsNot
+            }}
+        },
+        .run = EcsApplyTransformOnce3
     });
 }
